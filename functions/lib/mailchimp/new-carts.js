decf1afed6fae50cc113157db83b682daf870f71
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-08-30 23:31:51 -0300

    chore: check customer id before send

--- functions/lib/mailchimp/new-carts.js
+++ functions/lib/mailchimp/new-carts.js
@@ -9,53 +9,53 @@ module.exports = (cartId, storeId, appSdk, configObj) => {
       .apiRequest(storeId, url)
       .then(({ response }) => {
         const cartBody = response.data
-        const customerId = cartBody.customers[0]
-
-        const data = {
-          id: cartId,
-          customer: {
-            id: customerId
-          },
-          checkout_url: cartBody.permalink || '/app/#/checkout/' + cartId,
-          currency_code: 'BRL',
-          order_total: cartBody.subtotal || 0,
-          lines: []
-        }
-
-        const { items } = cartBody
-
-        if (configObj.mc_campaign_cart) {
-          data.campaign_id = configObj.mc_campaign_cart
-        }
-
-        if (items.length) {
-          items.forEach(item => {
-            data.lines.push({
-              id: item._id,
-              product_id: item.product_id,
-              quantity: item.quantity,
-              price: item.final_price || item.price,
-              product_variant_id: item.variation_id || item.product_id
-            })
-            if (data.order_total === 0) {
-              data.order_total += (item.final_price || item.price)
-            }
-          })
-        }
-
-        const mailchimp = new Mailchimp(configObj.mc_api_key)
-        mailchimp.get({
-          path: `/ecommerce/stores/${storeId}/carts/${cartId}`,
-        }).then(resp => {
-            if (cartBody.completed) {
-              mailchimp.delete({
-                path: `/ecommerce/stores/${storeId}/carts/${cartId}`,
-              }).then(response => {
-                console.log(`Deleted completed cart status: ${cartBody.completed} - ${cartId} - ${storeId}`)
-                return resolve(response)
+        const customerId = cartBody.customers && cartBody.customers.length && cartBody.customers[0]
+        if (customerId) {
+          const data = {
+            id: cartId,
+            customer: {
+              id: customerId
+            },
+            checkout_url: cartBody.permalink || '/app/#/checkout/' + cartId,
+            currency_code: 'BRL',
+            order_total: cartBody.subtotal || 0,
+            lines: []
+          }
+  
+          const { items } = cartBody
+  
+          if (configObj.mc_campaign_cart) {
+            data.campaign_id = configObj.mc_campaign_cart
+          }
+  
+          if (items.length) {
+            items.forEach(item => {
+              data.lines.push({
+                id: item._id,
+                product_id: item.product_id,
+                quantity: item.quantity,
+                price: item.final_price || item.price,
+                product_variant_id: item.variation_id || item.product_id
               })
-            }
-            return resolve(resp)
+              if (data.order_total === 0) {
+                data.order_total += (item.final_price || item.price)
+              }
+            })
+          }
+  
+          const mailchimp = new Mailchimp(configObj.mc_api_key)
+          mailchimp.get({
+            path: `/ecommerce/stores/${storeId}/carts/${cartId}`,
+          }).then(resp => {
+              if (cartBody.completed) {
+                mailchimp.delete({
+                  path: `/ecommerce/stores/${storeId}/carts/${cartId}`,
+                }).then(response => {
+                  console.log(`Deleted completed cart status: ${cartBody.completed} - ${cartId} - ${storeId}`)
+                  return resolve(response)
+                })
+              }
+              return resolve(resp)
           })
           .catch(error => {
             // not found
@@ -68,17 +68,28 @@ module.exports = (cartId, storeId, appSdk, configObj) => {
                   path: `/ecommerce/stores/${storeId}/carts`,
                   data
                 }).then(resp => {
-                  console.log(`Create new cart ${cartBody._id} | #${storeId}`)
-                  return resolve(resp)
-                }).catch(reject)
+                    console.log(`Create new cart ${cartBody._id} | #${storeId}`)
+                    return resolve(resp)
+                }).catch(err => {
+                    const { response } = err
+                    if (response.data && response.data.errors) {
+                      console.error('[!] INFO: ', JSON.stringify(response.data.errors, undefined, 2))
+                    }
+                    if (response.data && response.data.detail) {
+                      console.error('[!] DETAIL: ', response.data)
+                    }
+                    reject(err)
+                  })
               } else if (response.status && response.status === 400) {
-                // email adress 
-                reject(response)
+                  // email adress 
+                  reject(response)
               }
             } else {
               reject(error)
             }
           })
+        }
+        resolve(response)
       })
       .then(resolve)
       .catch(reject)
