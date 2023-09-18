8d59e03a79ad7408dcbc331711ec9233de33581e
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-09-18 20:54:46 -0300

    chore: update log to get trigger inserted id undefined

--- functions/routes/ecom/webhook.js
+++ functions/routes/ecom/webhook.js
@@ -34,7 +34,7 @@ exports.post = ({ appSdk }, req, res) => {
 
       const { resource } = trigger
       let promise = Promise.resolve()
-      console.log('Log trigger:', trigger.inserted_id, resource)
+      console.log('Log trigger:', trigger.inserted_id, resource, JSON.stringify(trigger))
       switch (resource) {
         case 'carts':
           const cartId = trigger.inserted_id
